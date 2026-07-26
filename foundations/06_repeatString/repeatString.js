de0c67b0c2936969b84ea1adaa2d5b08b3d1b13f
siva <sivaprakash.s4567@gmail.com>
2026-07-27 01:20:36 +0530

    finish 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str1,num) {
+    if (num<0){
+        return "ERROR";
+    }
+    let str2="";
+    for (let i=0;i<num;i++){
+        str2+=str1;
+    }
+    return str2
 };
+
+// console.log(repeatString('hey',3));
+// console.log(4);
 
 // Do not edit below this line
 module.exports = repeatString;
