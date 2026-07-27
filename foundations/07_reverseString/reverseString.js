cf4b3a49d5db2d10a554ba3199abb2cae1a6e1e4
siva <sivaprakash.s4567@gmail.com>
2026-07-27 21:48:54 +0530

    finish 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str1) {
+    let len =str1.length;
+    // console.log(len);
+    str2="";
+    for(let i=len-1; i>=0;i--){
+        str2+=str1[i];
+    }
+    return str2;
 };
 
+reverseString("abc");
 // Do not edit below this line
 module.exports = reverseString;
