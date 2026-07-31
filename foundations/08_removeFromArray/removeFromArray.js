3d2ab8263e3b934689a4aefe647d2a3ba0d74ff0
siva <sivaprakash.s4567@gmail.com>
2026-07-31 10:12:39 +0530

    complete 08_removefromarray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...nums) {
+    // console.log(num)
+    return arr
+        .filter((num)=> {
+            return !nums.includes(num);
+        });
+    
 };
 
+// removeFromArray(4,3,6);
+console.log(removeFromArray([1,2,5,96,6],96,2,7));
 // Do not edit below this line
 module.exports = removeFromArray;
