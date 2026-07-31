0060799245f361535055c1207e956eb3c7e686b5
siva <sivaprakash.s4567@gmail.com>
2026-07-31 10:41:36 +0530

    finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(n1,n2) {
+    const arr=[];
+    if(n1<0 || n2<0 || !Number.isInteger(n1) || !Number.isInteger(n2)){
+        return 'ERROR';
+    }
+    if (n1>n2){
+        let temp=n1;
+        n1=n2;
+        n2=temp;
+    }
+    for (let i=n1; i<=n2;i++){
+        arr.push(i);
+    }
 
+    return sum1 =arr.reduce((acc,cur)=>acc+cur,0);
+    
 };
 
+// console.log(sumAll(2,4));
 // Do not edit below this line
 module.exports = sumAll;
