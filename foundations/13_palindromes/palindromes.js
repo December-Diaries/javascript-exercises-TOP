a5e7185e168770aefca13d086401c688e76f8107
siva <sivaprakash.s4567@gmail.com>
2026-09-04 19:24:10 +0530

    complete 13_palindroes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let newtxt="";
+    for(const i of text.toLowerCase()){
+        // console.log(i);
+        
+        if (/^[a-z0-9]$/.test(i)){ 
+            newtxt+=i;
+        }
+        
+    }
+    return (newtxt === newtxt.split("").reverse().join(""));
+    
 };
 
+
+// console.log(palindromes('carrac'));
 // Do not edit below this line
 module.exports = palindromes;
